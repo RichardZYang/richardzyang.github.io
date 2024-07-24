6367ca372f997939180b8086e86d903463f1c9f0
RichardZYang <richardyang2468@gmail.com>
2024-07-24 17:19:42 +1000

    edited

---
+++ scripts/fadescript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  console.log("jQuery is loaded and ready");
+
+  function applyFading() {
+    var windowHeight = $(window).height();
+    var windowCenter = $(window).scrollTop() + (windowHeight / 2);
+
+    $(".animated-text").each(function() {
+      var $this = $(this);
+      var elementTop = $this.offset().top;
+      var elementHeight = $this.outerHeight();
+      var elementCenter = elementTop + (elementHeight / 2);
+
+      var distanceToCenter = Math.abs(windowCenter - elementCenter);
+      var maxDistance = windowHeight / 2;
+
+      var opacity = 1 - (distanceToCenter / maxDistance);
+      opacity = Math.max(0, Math.min(1, opacity));
+
+      $this.css("opacity", opacity);
+    });
+  }
+
+  $(window).on('scroll resize', function() {
+    applyFading();
+  });
+
+  applyFading(); // Initial check
+});
